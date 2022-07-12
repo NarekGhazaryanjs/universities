e2d2b55b09cc25137db8ce54320db91e66037c2c
Narek Ghazaryan <narek.ghazaryan@dataart.com>
2022-07-12 14:55:25 +0400

    done

--- univers/src/App.js
+++ univers/src/App.js
@@ -1,26 +1,47 @@
-import { useRef } from 'react/cjs/react.production.min';
+import { useRef, useState } from 'react';
 import './App.css';
 import Button from './components/Button/Button';
 import Card from './components/Card/Card';
 import Input from './components/Input/Input';
 import List from './components/List/List';
+import Listitem from './components/Listitem/Listitem';
 import Wrapper from './ui/Wrapper/Wrapper';
 
 const App = () => {
-  const searchCountryRef = useRef('')
+  const searchCountryRef = useRef('');
+  const [univers,setUnivers] = useState([]);
+  const api = 'http://universities.hipolabs.com/search?country=';
+
+
+ const searchUniversClickHandler = () => {
+    const countryKey = searchCountryRef.current.value
+    if(countryKey)  {
+      fetch(api + countryKey)
+      .then(data => data.json())
+      .then(univers => setUnivers(univers))
+    }
+ }
 
   return (
     <Wrapper>
-      <Card>
+      <Card className='header'>
         <Input propsRef={searchCountryRef} />
-        <Button> search universities </Button>
+        <Button onClick={searchUniversClickHandler}> search universities </Button>
       </Card>
 
-      <Card>
-        <List>
-  
+        <List className='container'> 
+          { univers.map((univer,index) => {
+            return (
+              <Listitem className='list-item ' key={index}>
+               country:  {univer.country} <br />
+               web-page: {univer.web_pages} <br />
+               alpha-code: {univer.alpha_two_code} <br />
+               domain: {univer.domains[0]}
+              </Listitem>
+            )
+          })}
         </List>
-      </Card>
+
     </Wrapper>
   )
 }
